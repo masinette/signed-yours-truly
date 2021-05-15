76e08e639a2d0a4a12a711476811d445f4332fc3
masinette <masinette.ts@gmail.com>
2021-05-15 17:25:28 -0400

    include link to bootstrap stylesheet in index head

--- signed-yours-truly/src/App.js
+++ signed-yours-truly/src/App.js
@@ -1,13 +1,16 @@
 import logo from './treble-clef.svg';
 import './App.css';
+import AlbumCard from './components/AlbumCard'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
       </header>
+
+      <AlbumCard />
+
     </div>
   );
 }
---
+++ signed-yours-truly/src/components/AlbumCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
+
+export default function AlbumCard() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col><h1>Album</h1></Col>
+        <Col>Col 2</Col>
+        <Col>Col 3</Col>
+
+      </Row>
+      
+    </Container>
+  )
+}
+
