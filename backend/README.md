252ec820f737e94078b6b53a5ba88e413e8bd8ad
masinette <masinette.ts@gmail.com>
2021-05-15 22:18:10 -0400

    set up server syt-api, and test api

--- backend/README.md
+++
@@ -1,1 +0,0 @@
-# Signed, Yours Truly
---
+++ frontend/src/components/AlbumCard.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Container, Row, Col, Card, Button} from 'react-bootstrap'
+import axios from 'axios';
+
+export default function AlbumCard() {
+
+  // useEffect(() => {
+  //   effect
+  //   return () => {
+  //     cleanup
+  //   }
+  // }, [input])
+
+
+  return (
+    <Container >
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="../logo.png/100px180" />
+        <Card.Body>
+          <Card.Title>Album Title</Card.Title>
+          <Card.Text>
+            This is where the album artist and tracks will go, and year
+          </Card.Text>
+          <Card.Footer>
+            <Card.Title>$100</Card.Title>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Footer>
+        </Card.Body>
+      </Card>
+    </Container>
+  )
+}
+
